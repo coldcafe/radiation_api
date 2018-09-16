77bd2820ba0fcdf931c00861c44814c625774e42
coldcofe <395134629@qq.com>
2018-09-17 00:08:03 +0800

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,6 @@
 import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Injectable, Inject } from '@nestjs/common';
-import * as qiniu from 'qiniu';
-import * as memoryCache from 'memory-cache';
 import config from './config';
 
 @Controller()
